f06591609e5eedaf2b1117b6759ac8bc751c5d3e
unknown <manjotnms@gmail.com>
2015-10-21 16:15:07 +0530

    Plugin added for motech alerts

---
+++ nagios_server/libexec/Motech_Alerts.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php -q
+<?php
+$Status=0;
+$Timeout=10;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER,0);
+curl_setopt($ch, CURLOPT_TIMEOUT, $Timeout);
+curl_setopt($ch, CURLOPT_URL,"http://192.168.200.1:8080/motech-platform-server/module/alerts/newAlertCount");
+
+if( ! $Buff=curl_exec($ch))
+{
+	echo "Error in Execution of Command";
+	$Status=1;
+}
+
+$reqInfo = curl_getinfo($ch);
+$errnum=curl_errno($ch);
+if ($errnum){
+	if ($errnum==28){
+        	echo 'Timeout '.$Timeout.'sec exceeded.';
+                exit(2);
+    	}else{
+               echo "ERROR in opening page! Err:".curl_error($ch);
+      	       exit(2);
+    }
+  }
+if ( $Buff > 0)
+{
+        echo "There are $Buff New Alerts";
+        $Status=1;
+}
+
+
+@curl_close($ch);
+exit($Status)
+?>
